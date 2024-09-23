44e1a17aa820c6c7c7fc46c759eca66b7d1d4d42
ZachRober <nolethedgehogdie@gmail.com>
2024-09-23 14:08:32 -0400

    Create department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -3,9 +3,24 @@ class Employee {
         this.name=name;
         this.salary=salary;
         this.position=position;
-        this.department=department;
-        getDetails();{
+        this.department=department;//object constructor
+    }
+        getDetails(){
 console.log(`${this.name} makes ${this.salary} working as a ${this.position} in the ${this.department} department`);
             return;
         }
-    }}
+    }
+class Department{
+    Constructor(name){
+    this.name = name;
+    this.employees = [];//empty array to add objects to
+    }
+    addEmployee(employee){
+        this.employees.push(employee)//method that pushes to employees array
+    }
+    getDepartmentSalary(deptName){
+        let total = 0;
+        this.name = deptName;
+        this.employees.forEach(x=>total+=x.salary);
+    }
+}
