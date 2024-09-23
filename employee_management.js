d214504a6ae7226d6d76b4180ba05080f60d8999
ZachRober <nolethedgehogdie@gmail.com>
2024-09-23 12:52:05 -0400

    create employee class with properties and methods

---
+++ employee_management.js
@@ -0,0 +1,11 @@
+class Employee {
+    Constructor(name,salary,position,department){
+        this.name=name;
+        this.salary=salary;
+        this.position=position;
+        this.department=department;
+        getDetails();{
+console.log(`${this.name} makes ${this.salary} working as a ${this.position} in the ${this.department} department`);
+            return;
+        }
+    }}
